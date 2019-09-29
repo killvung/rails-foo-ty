432f140550b970ac97113e84cb416de4c0a77197
Kiu Fung Lam <kiulam125@gmail.com>
2019-09-28 23:56:12 -0500

    Render splash draft
    Render clubs list

--- app/javascript/clubs.vue
+++ app/javascript/clubs.vue
@@ -1,23 +1,39 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
-    <ul id="example-1">
-      <li v-for="{name} in clubs">
-        {{ name }}
-      </li>
-    </ul>
+  <div class="ui container">
+    <div class="ui text">
+      <h1 class="ui inverted header">
+        <p>{{ message }}</p>
+      </h1>
+    </div>
+    <div class="ui items" v-for="({name}, index) in clubs" v-bind:key="index">
+      <div class="item">
+        <div class="image">
+          <img src="../assets/images/default.png" />
+        </div>
+        <div class="content">
+          <a class="header">{{name}}</a>
+          <div class="meta">
+            <span>Description</span>
+          </div>
+          <div class="description">
+            <p></p>
+          </div>
+          <div class="extra">Additional Details</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  data: function () {
+  data: function() {
     return {
-      message: "Clubs!"
-    }
+      message: "Clubs"
+    };
   },
-  props: ['clubs']
-}
+  props: ["clubs"]
+};
 </script>
 
 <style scoped>
