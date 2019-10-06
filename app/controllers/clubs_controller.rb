6ee8eff2eeac3e0c7b2aac59c8d9272d7518665d
Kiu Fung Lam <kiulam125@gmail.com>
2019-10-06 03:52:16 -0500

    Detail club information seperate players by level

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -6,6 +6,7 @@ class ClubsController < ApplicationController
 	def one 
 		@club = Club.find_by(id: params[:id])
 		@players = Player.where(club_id: params[:id])
+		@players_by_level = @players.group_by{|player| player.level}
 		raise ActionController::RoutingError.new('Not Found') if @club.nil?
 	end 
 end
